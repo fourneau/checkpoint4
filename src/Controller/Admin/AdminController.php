d9b344d27f903a9beb19f76fda22c5a3ef0916c9
fourneau <cathyjulien14@orange.fr>
2021-07-21 17:06:27 +0200

    avancébien

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -58,8 +58,8 @@ class AdminController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Compétences', 'fas fa-balance-scale', Expertise::class);
         yield MenuItem::linkToCrud('Actualités', 'fas fa-book-reader', News::class);
         yield MenuItem::linkToCrud('Catégories d\'actualités', 'fa fa-book fa-fw', NewsCategory::class);
-        yield MenuItem::section('Reglages');
         yield MenuItem::linkToCrud('Contact', 'fas fa-headset', Contact::class);
+        yield MenuItem::section('Reglages');
         yield MenuItem::linkToCrud('Carrousel', 'fas fa-parachute-box', UploadCarrousel::class);
         yield MenuItem::linkToCrud('Background', 'far fa-images', UploadBackground::class);
         yield MenuItem::linkToLogout('Deconnexion', 'fa fa-sign-out');
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -33,7 +33,7 @@ class ContactType extends AbstractType
         ('placeholder' => 'Message:', 'style' => 'height: 20rem;
         background-color:#ebebeb; border-radius:1rem')))
         ->add('checkbox', CheckboxType::class, [
-        'label' => 'En cochant cette case, j\'accepte que le cabinet me contacte pour répondre à ma demande',
+        'label' => 'En cochant cette case, j\'accepte de recevoir une Puncline de Julien',
         'required' => true,
         ])
         ;
